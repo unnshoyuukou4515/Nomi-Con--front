16aeab1b74006762915cc7e1673b22c1dce1f667
Koichiro Yokoi <112148201+unnshoyuukou4515@users.noreply.github.com>
2023-11-10 01:21:59 +0900

    fix:deleted console.log

--- solo-client/src/components/Login/Login.jsx
+++ solo-client/src/components/Login/Login.jsx
@@ -16,11 +16,11 @@ export default function LoginForm() {
       const apiUrl = import.meta.env.VITE_API_URL || "http://localhost:3000";
       const response = await axios.post(`${apiUrl}/login`, data);
       
-      console.log("Login response:", response);
-      console.log("User ID:", response.data.userId);
-      console.log("Username:", response.data.username);
+      // console.log("Login response:", response);
+      // console.log("User ID:", response.data.userId);
+      // console.log("Username:", response.data.username);
       if (response.status === 200) {
-        console.log("Login successful with data:", response.data);
+        // console.log("Login successful with data:", response.data);
         navigate('/home', { state: { userId: response.data.userId, username: response.data.username } });
       } else {
         setLoginStatus(response.status); 
